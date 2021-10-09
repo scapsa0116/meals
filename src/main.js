25cf9b0141f2df2b7239eafdb04402d3ecb5749b
stela <stela.capsa@gmail.com>
2021-10-09 23:36:43 +0300

    get elemenys by id

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
+const mealsEl = document.getElementById("meals");
+const favoriteContainer = document.getElementById("fav-meals");
+const mealPopup = document.getElementById("meal-popup");
+const mealInfoEl = document.getElementById("meal-info");
+const popupCloseBtn = document.getElementById("close-popup");
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
 getRandomMeal();
 
 async function getRandomMeal() {
-    const resp = await fetch(
+ fetch(
         "https://www.themealdb.com/api/json/v1/1/random.php"
     );    
     const randomMeal = await resp.json();
 
-    console.log(randomMeal)
+    console.log(randomMeal.meals[0])
 }
 
 async function getMealById(id){
