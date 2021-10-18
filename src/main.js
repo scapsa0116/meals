0aad9a5d7661384ea7135a41a25bd65c7d40c522
stela <stela.capsa@gmail.com>
2021-10-18 12:49:51 +0300

    css not working and dont know why

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-const mealsEl = document.getElementById("meals");
-const favoriteContainer = document.getElementById("fav-meals");
-const mealPopup = document.getElementById("meal-popup");
-const mealInfoEl = document.getElementById("meal-info");
-const popupCloseBtn = document.getElementById("close-popup");
-const searchTerm = document.getElementById("search-term");
-const searchBtn = document.getElementById("search");
-getRandomMeal();
+// const mealsEl = document.getElementById("meals");
+// const favoriteContainer = document.getElementById("fav-meals");
+// const mealPopup = document.getElementById("meal-popup");
+// const mealInfoEl = document.getElementById("meal-info");
+// const popupCloseBtn = document.getElementById("close-popup");
+// const searchTerm = document.getElementById("search-term");
+// const searchBtn = document.getElementById("search");
+// getRandomMeal();
 
-async function getRandomMeal() {
-    const resp = await fetch(
-        "https://www.themealdb.com/api/json/v1/1/random.php"
-    );
+// async function getRandomMeal() {
+//     const resp = await fetch(
+//         "https://www.themealdb.com/api/json/v1/1/random.php"
+//     );
     
-    const resp = await resp.json();
-    const randomMeal = respData.meals[0];
+//     // const resp = await resp.json();
+//     const randomMeal = respData.meals[0];
 
-    console.log(randomMeal.meals[0])
-}
+//     console.log(randomMeal.meals[0])
+// }
 
-async function getMealById(id){
-    const meal = await fetch("www.themealdb.com/api/json/v1/1/lookup.php?i="+id)
-}
+// async function getMealById(id){
+//     const meal = await fetch("www.themealdb.com/api/json/v1/1/lookup.php?i="+id)
+// }
 
-async function getMealsBySearch(term){
-    const meals = await fetch("www.themealdb.com/api/json/v1/1/search.php?s="+term)
-}
+// async function getMealsBySearch(term){
+//     const meals = await fetch("www.themealdb.com/api/json/v1/1/search.php?s="+term)
+// }
